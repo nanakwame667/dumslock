b5501e0c149d05517400ee55e3fc864200594795
Nanakwame <44173438+nanakwame667@users.noreply.github.com>
2022-10-18 22:26:55 +0000

    added sponsors

--- src/components/Sponsor/Sponsor.tsx
+++ src/components/Sponsor/Sponsor.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Sponsor.css'
+import google from '../../assets/images/google.svg';
+import meta from '../../assets/images/meta.svg';
+import aws from '../../assets/images/aws.svg';
 
 const Sponsor = () => {
   return (
-    <div>Sponsor</div>
+    <div className='container sponsor'>
+        <h1> trusted & accredited </h1>
+              <ul className='sponsors'>
+                  <li><Link to='/'><img src={google} alt="google" /></Link></li>
+                  <li><Link to='/'><img src={meta} alt="meta" /></Link></li>
+                  <li><Link to='/'><img src={aws} alt="aws" /></Link></li>
+              </ul>
+    </div>
   )
 }
 
